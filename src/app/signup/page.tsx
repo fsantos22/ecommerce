1bc462ccffcdde10d86998032ba4750f9c57314a
fsantos22 <22fsantos@gmail.com>
2024-04-04 12:03:51 -0300

    feat: implement business rules on frontend

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-
+import { zodResolver } from "@hookform/resolvers/zod";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Alert from "@mui/material/Alert";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -8,10 +9,70 @@ import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
+import Snackbar from "@mui/material/Snackbar";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 export default function SignUp() {
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+
+  const userSchema = z
+    .object({
+      firstName: z
+        .string()
+        .min(2, { message: "O nome deve ter pelo menos 2 caracteres" })
+        .max(30, { message: "O nome deve ter no máximo 30 caracteres" }),
+      lastName: z
+        .string()
+        .min(2, { message: "O nome deve ter pelo menos 2 caracteres" })
+        .max(60, { message: "O nome deve ter no máximo 60 caracteres" }),
+      email: z.string().email({ message: "E-mail inválido" }),
+      password: z
+        .string()
+        .min(5, { message: "O nome deve ter pelo menos 5 caracteres" })
+        .max(15, { message: "O nome deve ter no máximo 15 caracteres" }),
+      confirmPassword: z
+        .string()
+        .min(5, { message: "O nome deve ter pelo menos 5 caracteres" })
+        .max(15, { message: "O nome deve ter no máximo 15 caracteres" }),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "As senhas não são iguais",
+      path: ["confirmPassword"],
+    });
+
+  type UserSchema = z.infer<typeof userSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserSchema>({
+    resolver: zodResolver(userSchema),
+  });
+
+  const handleSubmitUser = async (data: UserSchema) => {
+    try {
+      const endpoint = "/api/users";
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      const response = await fetch(endpoint, options);
+      if (response.ok) {
+        setIsAlertOpen(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -23,6 +84,18 @@ export default function SignUp() {
           alignItems: "center",
         }}
       >
+        {isAlertOpen && (
+          <Snackbar
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            open={isAlertOpen}
+            autoHideDuration={2000}
+            onClose={() => setIsAlertOpen(false)}
+          >
+            <Alert severity="success" color="success" sx={{ width: "100%" }}>
+              Cadastro realizado com sucesso!
+            </Alert>
+          </Snackbar>
+        )}
         <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
           <LockOutlinedIcon />
         </Avatar>
@@ -30,7 +103,11 @@ export default function SignUp() {
           Cadastrar acesso
         </Typography>
 
-        <Box component="form" onSubmit={} sx={{ mt: 3 }}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(handleSubmitUser)}
+          sx={{ mt: 3 }}
+        >
           <Grid container spacing={3}>
             <Grid item xs={12}>
               <TextField
@@ -39,13 +116,32 @@ export default function SignUp() {
                 fullWidth
                 id="firstName"
                 label="Nome"
+                {...register("firstName")}
+                error={errors.firstName ? true : false}
+                helperText={errors.firstName?.message}
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField required fullWidth id="lastName" label="Sobrenome" />
+              <TextField
+                required
+                fullWidth
+                id="lastName"
+                label="Sobrenome"
+                {...register("lastName")}
+                error={errors.lastName ? true : false}
+                helperText={errors.lastName?.message}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField required fullWidth id="email" label="E-mail" />
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="E-mail"
+                {...register("email")}
+                error={errors.email ? true : false}
+                helperText={errors.email?.message}
+              />
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -54,6 +150,9 @@ export default function SignUp() {
                 label="Senha"
                 type="password"
                 id="password"
+                {...register("password")}
+                error={errors.password || errors.confirmPassword ? true : false}
+                helperText={errors.password?.message}
               />
             </Grid>
             <Grid item xs={12}>
@@ -63,6 +162,9 @@ export default function SignUp() {
                 label="Confirmar senha"
                 type="confirmPassword"
                 id="confirmPassword"
+                {...register("confirmPassword")}
+                error={errors.confirmPassword ? true : false}
+                helperText={errors.confirmPassword?.message}
               />
             </Grid>
           </Grid>
