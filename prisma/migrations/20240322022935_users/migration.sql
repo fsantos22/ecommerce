31e469f1a1150edf510a502d8a195fa53b4f5a52
fsantos22 <22fsantos@gmail.com>
2024-04-04 11:58:09 -0300

    chore: create db and prisma integration

---
+++ prisma/migrations/20240322022935_users/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
