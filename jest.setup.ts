80a1bc6a678468270481ec51021ae570e72ef86b
fsantos22 <22fsantos@gmail.com>
2024-04-25 13:33:08 -0300

    chore(config files): config prettier, eslint, husky, jest, commitlint, commitzen

---
+++ jest.setup.ts
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom'
+
+// import { server } from './mocks/server';
+
+// beforeAll(()=>{
+//     server.listen()
+// })
+
+// beforeEach(()=>{
+
+// })
+
+// afterAll(()=>{
+//     server.close()
+// })
+
+// afterEach(()=>{
+//     server.resetHandlers()
+// })
