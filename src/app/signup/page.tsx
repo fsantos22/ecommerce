2d59e045add0a8157a762f067a4208e52eb7a062
fsantos22 <22fsantos@gmail.com>
2024-04-30 11:38:44 -0300

    test(signup): tests

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,34 +15,9 @@ import Typography from '@mui/material/Typography'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-
+import {userSchema} from '@/schemas/user'
 export default function SignUp() {
   const [isAlertOpen, setIsAlertOpen] = useState(false)
-
-  const userSchema = z
-    .object({
-      firstName: z
-        .string()
-        .min(2, { message: 'O nome deve ter pelo menos 2 caracteres' })
-        .max(30, { message: 'O nome deve ter no máximo 30 caracteres' }),
-      lastName: z
-        .string()
-        .min(2, { message: 'O sobrenome deve ter pelo menos 2 caracteres' })
-        .max(60, { message: 'O sobrenome deve ter no máximo 60 caracteres' }),
-      email: z.string().email({ message: 'E-mail inválido' }),
-      password: z
-        .string()
-        .min(5, { message: 'A senha deve ter pelo menos 5 caracteres' })
-        .max(15, { message: 'A senha deve ter no máximo 15 caracteres' }),
-      confirmPassword: z
-        .string()
-        .min(5, { message: 'A senha deve ter pelo menos 5 caracteres' })
-        .max(15, { message: 'A senha deve ter no máximo 15 caracteres' }),
-    })
-    .refine((data) => data.password === data.confirmPassword, {
-      message: 'As senhas não são iguais',
-      path: ['confirmPassword'],
-    })
 
   type UserSchema = z.infer<typeof userSchema>
 
